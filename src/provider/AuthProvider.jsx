01839784017bf829eb12b7554ece6015da617569
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-05-05 18:24:48 +0600

    added user functionality to the register form

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,14 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
 
-const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "steven",
-    email: "steven@smith.com",
-  });
+const auth = getAuth(app);
 
-  const authData = { user, setUser };
+const AuthProvider = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const [user, setUser] = useState(null);
+  console.log(user);
+
+  const authData = { user, setUser, createUser };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
