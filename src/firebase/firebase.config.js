9babcb5ea7281dd5afc810b879aab87a69ca0ef9
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-05-05 17:39:12 +0600

    added context api

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCP8p-c0UpX0p5WuCOc1hM0UqAnvooQee8",
+  authDomain: "dragon-news-breaking-da6f8.firebaseapp.com",
+  projectId: "dragon-news-breaking-da6f8",
+  storageBucket: "dragon-news-breaking-da6f8.firebasestorage.app",
+  messagingSenderId: "323237361894",
+  appId: "1:323237361894:web:ff229100dae7aa4702e327",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
